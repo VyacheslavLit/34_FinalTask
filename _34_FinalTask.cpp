a112a51dcf20d79e20f652fc7165fea82ee12dfa
VyacheslavLit <kinglva@yandex.ru>
2023-08-17 18:12:19 +0300

    Add files via upload

---
+++ _34_FinalTask.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+template <typename T> concept ComplexConcept = requires(T value)
+{
+	{value.tostring() } -> std::same_as<std::string>;
+	{value.hash()} -> std::convertible_to<long>;
+		requires !std::has_virtual_destructor<T>::value;
+};
+
+//провкрка концепта
+class ComplexCon {
+private:
+	short _n = 0;
+public:
+	ComplexCon(short n) : _n(n) {};
+	~ComplexCon() {};
+
+	std::string tostring() const {return std::to_string (_n);}
+
+	long hash() const {return (long)_n;}
+};
+
+
+int main()
+{
+	ComplexCon x{ 5 };
+	std::cout << typeid(x.hash()).name() << std::endl;
+	std::cout << typeid(x.tostring()).name() << std::endl;
+
+	return 0;
+}
